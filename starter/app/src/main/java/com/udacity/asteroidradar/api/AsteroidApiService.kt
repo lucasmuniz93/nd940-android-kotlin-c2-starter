61284f38aec79359a867d5f64a957db8dc3ee6cc
Lucas Muniz <lucasmuniz@id.uff.br>
2020-11-02 22:28:06 -0300

    Add the JSON parse

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/api/AsteroidApiService.kt
@@ -0,0 +1,39 @@
+package com.udacity.asteroidradar.api
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.Constants.BASE_URL
+import kotlinx.coroutines.Deferred
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val API_KEY = "api_key=vKDcxvszV9311kvSweniUYEn3lR6WT8Quguall3P"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(BASE_URL)
+    .build()
+
+
+interface AsteroidApiService {
+    @GET("neo/rest/v1/feed?start_date=2020-11-02&end_date=2020-11-09&$API_KEY")
+     fun getProperties(): Deferred<String>
+
+    @GET("planetary/apod?$API_KEY")
+    fun getImageOfTheDay(): Call<String>
+}
+
+object AsteroidApi {
+    val retrofitService: AsteroidApiService by lazy {
+        retrofit.create(AsteroidApiService::class.java)
+    }
+}
--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,66 @@
 package com.udacity.asteroidradar.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.api.AsteroidApi
+import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.await
+import java.lang.Exception
 
 class MainViewModel : ViewModel() {
+
+    private val _status = MutableLiveData<String>()
+    val status: LiveData<String>
+        get() = _status
+
+    private val _imageOfDay = MutableLiveData<String>()
+    val imageOfDay: LiveData<String>
+        get() = _imageOfDay
+
+    private val _asteroids = MutableLiveData<List<Asteroid>>()
+    val asteroid: LiveData<List<Asteroid>>
+        get() = _asteroids
+
+    init {
+        getAsteroidsRealEstateProperties()
+    }
+
+    // Get and parse the JSON from API
+    private fun getAsteroidsRealEstateProperties() {
+        viewModelScope.launch {
+            try {
+                val restult = AsteroidApi.retrofitService.getProperties().await()
+                _asteroids.value = parseAsteroidsJsonResult(JSONObject(restult))
+            } catch (e: Exception) {
+                _status.value = "Failure: ${e.message}"
+            }
+        }
+    }
+
+    private fun getImageOfTheDay() {
+
+        viewModelScope.launch {
+
+            AsteroidApi.retrofitService.getImageOfTheDay().enqueue(object : Callback<String> {
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    Log.i("Mylog ", "Success Image")
+                }
+
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    Log.i("Mylog Failed Image:", t.message.toString())
+                }
+
+            })
+
+        }
+    }
 }
