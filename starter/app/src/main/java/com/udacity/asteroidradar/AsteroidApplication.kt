6d4d3aeff7f8b4a61656358b382d8aa4e6753db2
Lucas Muniz <lucasmuniz@id.uff.br>
2020-11-04 21:25:58 -0300

    Add background work

---
+++ starter/app/src/main/java/com/udacity/asteroidradar/AsteroidApplication.kt
@@ -0,0 +1,43 @@
+package com.udacity.asteroidradar
+
+import android.app.Application
+import androidx.constraintlayout.solver.state.State
+import androidx.work.*
+import com.udacity.asteroidradar.work.RefreshDataWork
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+
+class AsteroidApplication : Application() {
+
+    val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            startWork()
+        }
+    }
+
+    // Config the constraints to require network and charging
+    private fun startWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresCharging(true).build()
+
+        val repeatingRequest
+                = PeriodicWorkRequestBuilder<RefreshDataWork>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            RefreshDataWork.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest)
+    }
+}
